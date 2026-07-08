a9efdb2a5310e90004c579795b1a7b9151390967
Thiago Castagnoli <murilothiago813@gmail.com>
2026-07-08 20:17:34 -0300

    feat: adiciona tela de Salvos ao BottomNavigation com navegação e interface

---
+++ app/src/main/java/com/example/smolkline/Fragments/SalvosFragment.kt
@@ -0,0 +1,20 @@
+package com.example.smolkline.Fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smolkline.Model.Mollo
+import com.example.smolkline.R
+import com.example.smolkline.adapters.MolloAdapter
+
+public class SalvosFragment: Fragment(R.layout.salvos_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+    }
+}
